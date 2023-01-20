e853527f6abc67e7d129646e4564d5906ea544b6
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-20 14:48:52 -0500

    Add unittests for percent_width and height

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,15 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from src.utils import *
+
+
+class TestCalcPercentHeight(unittest.TestCase):
+    def test_returns_120(self):
+        self.assertEqual(calc_percent_length(25, 480), 120)
+
+    def test_returns_240(self):
+        self.assertEqual(calc_percent_length(50, 480), 240)
+
 
 if __name__ == '__main__':
     unittest.main()
